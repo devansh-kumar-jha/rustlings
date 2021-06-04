45737df0bbcf160c41224a5b29ab22ed9abdee3e
Devansh <devansh.kujha2003@gmail.com>
2021-06-04 21:16:26 +0530

    strings

--- exercises/strings/strings1.rs
+++ exercises/strings/strings1.rs
@@ -6,9 +6,9 @@
 
 fn main() {
     let answer = current_favorite_color();
-    println!("My current favorite color is {}", answer);
+    println!("My current favorite color is {}", &answer);
 }
 
 fn current_favorite_color() -> String {
-    "blue"
+    String::from("blue")
 }
